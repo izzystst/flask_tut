7c2d49424a356c2cdc5a1d9a841c4b4ad4d98b41
Izzy <IZZZZZY@Isobellas-MBP-3.attlocal.net>
2020-09-15 11:57:52 -0500

    first vid

--- flaskblog.py
+++ flaskblog.py
@@ -2,5 +2,16 @@ from flask import Flask
 app = Flask(__name__) # name is just the name of the module - can be == to main 
 
 @app.route("/")
-def hello():
-	return "hello world"
+@app.route('/home')
+def home():
+	return "<h1>Home Page<h1>"
+@app.route("/about")
+def about():
+	return "<h1>About Page<h1>"
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+# https://www.youtube.com/watch?v=QnDWIZuWYW0
